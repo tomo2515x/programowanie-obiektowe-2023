ee61a2e3a5cc067f5a19d4beba5b5d3c77320458
tomo2515x <tomo2515pl@gmail.com>
2024-02-17 13:04:37 +0100

    feat: update

---
+++ src/z9_chargers/Car.py
@@ -0,0 +1,6 @@
+class Car:
+    def __init__(self, vin, total_charged_kwh, max_current_kw):
+        self.vin = vin
+        self.total_charged_kwh = total_charged_kwh
+        self.max_current_kw = max_current_kw
+
